c8f9e9a718516fbfc70d1b4cadee5665f13f50de
Caleb Moore <ctmoore@smu.edu>
2021-11-15 17:14:07 -0600

    initial push

---
+++ cmake-build-debug/CMakeFiles/21f_pa03_caleb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "21f_pa03_caleb"
+  "21f_pa03_caleb.pdb"
+  "CMakeFiles/21f_pa03_caleb.dir/src/graf.cpp.o"
+  "CMakeFiles/21f_pa03_caleb.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/21f_pa03_caleb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
